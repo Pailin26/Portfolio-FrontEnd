118d5c76dc47dbeb8bead9fe17a6a0257f683f7c
Pailin26 <63011233@kmitl.ac.th>
2022-11-27 22:54:44 +0700

    1st commit

---
+++ js/about.js
@@ -0,0 +1,58 @@
+
+// show/hide nav menu
+const menu = document.querySelector(".nav__menu");
+const menuBtn = document.querySelector("#open-menu-btn");
+const closeBtn = document.querySelector("#close-menu-btn");
+const themeToggle = document.querySelector(".theme-toggle");
+
+
+menuBtn.addEventListener('click', () => {
+  menu.style.display = "flex";
+  closeBtn.style.display = "inline-block";
+  menuBtn.style.display = "none";
+})
+
+
+// close nav menu
+const closeNav = () => {
+  menu.style.display = "none";
+  closeBtn.style.display = "none";
+  menuBtn.style.display = "inline-block";
+}
+
+closeBtn.addEventListener('click', closeNav)
+
+themeToggle.addEventListener('click', () => {
+  document.body.classList.toggle('dark-theme-variables');
+
+  themeToggle.querySelector('span:nth-child(1)').classList.toggle('active');
+  themeToggle.querySelector('span:nth-child(2)').classList.toggle('active');
+
+  if(document.body.classList.contains('dark-theme-variables')) {
+    
+    console.log("DarkMode")
+    theme = "DARK" ;
+
+  }else{
+    
+    console.log("LightMode")
+    theme = "LIGHT" ;
+  }
+
+  // save localStorage
+
+  localStorage.setItem("PageTheme",JSON.stringify(theme));
+
+
+
+
+})
+
+let GetTheme = JSON.parse(localStorage.getItem("PageTheme"));
+console.log(GetTheme);
+
+if(GetTheme === "DARK"){
+  themeToggle.querySelector('span:nth-child(1)').classList.toggle('visible');
+  themeToggle.querySelector('span:nth-child(2)').classList.toggle('active');
+  document.body.classList = ('dark-theme-variables');
+}
